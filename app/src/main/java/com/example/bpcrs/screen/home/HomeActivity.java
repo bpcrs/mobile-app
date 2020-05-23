760d10859f53532a5b2890e51e4e6622fd6c15a4
TienNDSE62860 <44231091+TienNDSE62860@users.noreply.github.com>
2020-05-23 19:27:29 +0700

    add track location

--- app/src/main/java/com/example/bpcrs/screen/home/HomeActivity.java
+++ app/src/main/java/com/example/bpcrs/screen/home/HomeActivity.java
@@ -1,10 +1,17 @@
 package com.example.bpcrs.screen.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.bpcrs.R;
+import com.example.bpcrs.screen.home.car.CarFragment;
+import com.example.bpcrs.screen.home.contract.ContractFragment;
+import com.example.bpcrs.screen.home.tracking.TrackingFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -17,6 +24,33 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         bottomNavigationView = findViewById(R.id.bot_navigate_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CarFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_car:
+                            selectedFragment = new CarFragment();
+                            break;
+                        case R.id.nav_contract:
+                            selectedFragment = new ContractFragment();
+                            break;
+                        case R.id.nav_tracking:
+                            selectedFragment = new TrackingFragment();
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+
+    public void trackingMyCar(View view) {
 
     }
 }
---
+++ app/src/main/java/com/example/bpcrs/screen/home/tracking/TrackingFragment.java
@@ -0,0 +1,83 @@
+package com.example.bpcrs.screen.home.tracking;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bpcrs.R;
+import com.example.bpcrs.screen.track.TrackActivity;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TrackingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TrackingFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private CardView cvTrack;
+
+    public TrackingFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment TrackingFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TrackingFragment newInstance(String param1, String param2) {
+        TrackingFragment fragment = new TrackingFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        cvTrack = getActivity().findViewById(R.id.cv_track);
+        cvTrack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), TrackActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tracking, container, false);
+    }
+}
